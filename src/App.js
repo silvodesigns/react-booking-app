e912822d1f184f02af632739c17ebb5141d1d172
Kevin Silvestre <kevsilvo@gmail.com>
2022-04-30 15:05:46 -0400

    Building Nav Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/single/Hotel";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/hotels" element={<List />}></Route>
+        <Route path="/hotels/:id" element={<Hotel />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/single/Hotel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Hotel = () => {
+  return <div>single hotel</div>;
+};
+
+export default Hotel;
